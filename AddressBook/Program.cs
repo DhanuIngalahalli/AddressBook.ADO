1aea72b49ab105a79fa3cc4e0586a6f824ce50a4
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-10-26 06:02:48 -0700

    Adding database for AddressBook

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,7 +6,21 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook");
+            Console.WriteLine("AddressBook Ado.net ");
+            PersonRepo repo = new PersonRepo();
+            PersonModel employee = new PersonModel();
+            employee.FirstName = "Pooja";
+            employee.LastName = "shetty";
+            employee.Address = "House no 22";
+            employee.PhoneNumber = "6302907678";
+            employee.zip = 894523;
+            employee.City = "Tsk";
+            employee.State = "Tripura";
+            employee.Email = "pooja@gmail.com";
+            employee.AddressBookName = "book4";
+            employee.Type = "Family";
+
+            repo.AddEmployee(employee);
         }
     }
 }
